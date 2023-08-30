f0d6d14a24ef523e32fe678f4e9c809a0b7dd0d4
Matheus Koyama <koyamaCRS@gmail.com>
2023-08-30 18:40:42 -0300

    Atualiza CSS e HTML

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+ 
+  name = 'Matheus';
+  age: number = 30;
+  job = 'Programador';
 
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 }
